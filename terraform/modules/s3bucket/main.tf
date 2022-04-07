4b32b6744f4d3b33b36ef6cd419840f377708511
Ash Gurung <agurung1919@gmail.com>
2022-04-07 11:24:15 +0100

    bucket

---
+++ terraform/modules/s3bucket/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "ef-terraform-state"
+    key    = "TF_logs"
+    region = "eu-west-2"
+  }
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-tf-test-bucket-lsjflsjfshfshfs"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
